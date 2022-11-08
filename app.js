2299dd3f556705d573ec75c4d3fd1c2a6288d502
kiyomaron <kyn.maron947@gmail.com>
2022-11-08 21:18:55 +0900

    Install EJS

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.get("/", function(req, res) {
   res.send("Hello");
 })
 
+let today = new Date();
+let currentDay = today.getDay();
+
+if (currentDay === 6 || currentDay === 0) {
+  res.write("<h1>Yeeeey! It's weekend!!</h1>");
+} else {
+  res.sendFile(__dirname + "/index.html");
+}
+
 app.listen(3000, function() {
   console.log("Server is running on 3000.");
 })
