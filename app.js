2a0b896a4184d54533a44b39803f12d5d09563a1
kiyomaron <kyn.maron947@gmail.com>
2022-11-09 21:05:58 +0900

    Add work to do list

--- app.js
+++ app.js
@@ -1,20 +1,66 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express;
+const app = express();
+
+let items = [];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.send("Hello");
+
+
+  let today = new Date();
+  let currentDay = today.getDay();
+
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("ja-JP", options);
+
+  // let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  // day = days[currentDay];
+
+  res.render("list", {listTitle: day, newListItems: items});
+
+});
+
+app.post("/", function(req,res) {
+  let item = req.body.newItem;
+
+  if(req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+
+  // console.log(req.body);
+
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
 })
 
-let today = new Date();
-let currentDay = today.getDay();
-
-if (currentDay === 6 || currentDay === 0) {
-  res.write("<h1>Yeeeey! It's weekend!!</h1>");
-} else {
-  res.sendFile(__dirname + "/index.html");
-}
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
 
 app.listen(3000, function() {
   console.log("Server is running on 3000.");
